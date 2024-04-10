54e7949393e864d22001d156d156c55a04680657
Emalyanov535 <nouctanch777093@gmail.com>
2024-04-10 14:02:19 +0400

    initial project

---
+++ src/main/java/ru/swe/skywingsexpressserver/SkyWingsExpressServerApplication.java
@@ -0,0 +1,13 @@
+package ru.swe.skywingsexpressserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyWingsExpressServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyWingsExpressServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/swe/skywingsexpressserver/SkyWingsExpressServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.swe.skywingsexpressserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyWingsExpressServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
