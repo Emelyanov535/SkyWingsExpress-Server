24192205724be1e90b40c8f791674138cc15d097
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2024-06-04 18:01:02 +0400

    feature: google login dosnt work

--- src/main/java/ru/swe/skywingsexpressserver/configuration/SecurityConf.java
+++ src/main/java/ru/swe/skywingsexpressserver/configuration/SecurityConf.java
@@ -6,14 +6,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 public class SecurityConf {
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConf(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -27,6 +37,15 @@ public class SecurityConf {
                         .failureUrl("/login-failure")
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()))
                 .build();
+    }
+
+    @Bean
+    public LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/");
+        return successHandler;
     }
 }
