3fb83140e99240b675b8c16e424d82f7c93f2d4d
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2024-06-04 23:21:31 +0400

    feature: convert to dto

--- src/main/java/ru/swe/skywingsexpressserver/service/FlightFilterService.java
+++ src/main/java/ru/swe/skywingsexpressserver/service/FlightFilterService.java
@@ -2,21 +2,28 @@ package ru.swe.skywingsexpressserver.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.swe.skywingsexpressserver.dto.flight.FlightDto;
 import ru.swe.skywingsexpressserver.model.operator.FlightModel;
 import ru.swe.skywingsexpressserver.repository.operator.FlightRepository;
+import ru.swe.skywingsexpressserver.utils.DtoModelMapper;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class FlightFilterService {
 
     private final FlightRepository flightRepository;
+    private final DtoModelMapper mapper;
 
-    public List<FlightModel> getFlightsByRouteAndDate(String origin, String destination, LocalDateTime startDate) {
-        return flightRepository.findByRouteOriginAndRouteDestinationAndDepartureTime(origin, destination, startDate);
+    public List<FlightDto> getFlightsByRouteAndDate(String origin, String destination, LocalDateTime startDate) {
+        var flights = flightRepository.findByRouteOriginAndRouteDestinationAndDepartureTime(origin, destination, startDate);
+        return flights.stream()
+                .map(flight -> mapper.transform(flight, FlightDto.class))
+                .collect(Collectors.toList());
     }
 
     public List<FlightModel> getFlightsSortedByPrice() {
