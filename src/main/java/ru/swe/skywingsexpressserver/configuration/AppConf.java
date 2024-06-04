74a084223295dd6f0b65ac219214fab6271d424e
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2024-06-04 19:33:31 +0400

    feature: search edited

--- src/main/java/ru/swe/skywingsexpressserver/configuration/AppConf.java
+++ src/main/java/ru/swe/skywingsexpressserver/configuration/AppConf.java
@@ -3,11 +3,16 @@ package ru.swe.skywingsexpressserver.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class AppConf {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public DateTimeFormatter dateTimeFormatter() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     }
 }
