0a5bffc2fb6b66f0ef4666cb8cc7e67827c09dc1
Emalyanov535 <nouctanch777093@gmail.com>
2024-06-07 15:17:24 +0400

    feature: correct work dto mapping

--- src/main/java/ru/swe/skywingsexpressserver/dto/flight/FlightDto.java
+++ src/main/java/ru/swe/skywingsexpressserver/dto/flight/FlightDto.java
@@ -16,54 +16,5 @@ public record FlightDto(
     Integer availableSeats,
     BigDecimal ticketPrice,
     Double discountPercentage
-) implements BaseFlightDto {
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public String getFlightNumber() {
-        return flightNumber;
-    }
-
-    @Override
-    public RouteDto getRoute() {
-        return route;
-    }
-
-    @Override
-    public AirlineDto getAirline() {
-        return airline;
-    }
-
-    @Override
-    public LocalDateTime getDepartureTime() {
-        return departureTime;
-    }
-
-    @Override
-    public LocalDateTime getArrivalTime() {
-        return arrivalTime;
-    }
-
-    @Override
-    public Integer getTotalSeats() {
-        return totalSeats;
-    }
-
-    @Override
-    public Integer getAvailableSeats() {
-        return availableSeats;
-    }
-
-    @Override
-    public BigDecimal getTicketPrice() {
-        return ticketPrice;
-    }
-
-    @Override
-    public Double getDiscountPercentage() {
-        return discountPercentage;
-    }
+) {
 }
--- src/main/java/ru/swe/skywingsexpressserver/dto/flight/FlightsResponseDto.java
+++ src/main/java/ru/swe/skywingsexpressserver/dto/flight/FlightsResponseDto.java
@@ -3,9 +3,8 @@ package ru.swe.skywingsexpressserver.dto.flight;
 import java.util.List;
 
 public record FlightsResponseDto(
-    List<BaseFlightDto> departureFlights,
-    List<BaseFlightDto> connectingFlights,
-    List<BaseFlightDto> returnFlights
+    List<FlightDto> departureFlights,
+    List<FlightDto> returnFlights
 ){
 
 }
