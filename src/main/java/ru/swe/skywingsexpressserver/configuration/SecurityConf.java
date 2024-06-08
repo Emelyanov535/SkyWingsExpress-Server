59cfc0b63c988d6b520e31d77d4ee7d85dee80c0
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2024-06-09 03:48:01 +0400

    feature: add new config for correct fetches

--- src/main/java/ru/swe/skywingsexpressserver/configuration/SecurityConf.java
+++ src/main/java/ru/swe/skywingsexpressserver/configuration/SecurityConf.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,7 +28,8 @@ public class SecurityConf {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(CsrfConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(c -> c
                         //.requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
