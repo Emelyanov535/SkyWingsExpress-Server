72ebe1f9c75794ce5d7bad25b042dd7b53b8f803
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2024-06-16 18:31:03 +0400

    fix Role

--- src/main/java/ru/swe/skywingsexpressserver/model/user/RoleModel.java
+++ src/main/java/ru/swe/skywingsexpressserver/model/user/RoleModel.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.swe.skywingsexpressserver.model.RoleEnum;
 
 @Data
 @Entity
